27d3705af736c77b860815b065701658eb5f3267
Emil Jönsson <ungothar97@gmail.com>
2020-11-13 09:42:24 +0100

    Added movement

--- snakeScript.js
+++ snakeScript.js
@@ -5,15 +5,35 @@ const snake_col = 'green';
 const snake_border = 'black';
 const board = document.getElementById("gameCanvas");
 const board_ctx = gameCanvas.getContext("2d");
+var gridSize = 20;
+var updateTime = 100;
 
-let snake = [{ x: 200, y: 200 }, { x: 190, y: 200 }, {   x: 180, y: 200 }];
+let snake = [
+    { x: 200, y: 200 },
+    { x: 200 - gridSize, y: 200 },
+    { x: 200 - gridSize*2, y: 200 },
+    { x: 200 - gridSize*3, y: 200 },
+    { x: 200 - gridSize*4, y: 200 }
+]
 
+let dx = 20;
+let dy = 0;
 main();
 
 // main function called repeatedly to keep the game running
 function main() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {
+        clearCanvas();
+        move();
+        draw();
+        main();
+    }, updateTime)
+    updateTime = snake.length*5 + 50;
+}
+function move() {
+    var head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(head);
+    snake.pop();
 }
 
 
@@ -32,11 +52,11 @@ function clearCanvas() {
 function drawSnakePart(snakePart) {
     board_ctx.fillStyle = 'lightblue';
     board_ctx.strokestyle = 'darkblue';
-    board_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-    board_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+    board_ctx.fillRect(snakePart.x, snakePart.y, gridSize, gridSize);
+    board_ctx.strokeRect(snakePart.x, snakePart.y, gridSize, gridSize);
 }
 
 /*Function that prints the parts*/
-function drawSnake() {
+function draw() {
     snake.forEach(drawSnakePart);
 }
