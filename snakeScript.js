a1559207dc9802a498d174ede0fb94905bd0429e
Emil Jönsson <ungothar97@gmail.com>
2020-11-25 18:10:21 +0100

    Added WASD-keys and boundaries

--- snakeScript.js
+++ snakeScript.js
@@ -13,29 +13,49 @@ const KEY_UP = 38;
 const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
 
-var gridSize = 20;
-var updateTime = 100;
+const KEY_W = 87;
+const KEY_A = 65;
+const KEY_S = 83;
+const KEY_D = 68;
 
-let snake = [
-    { x: 200, y: 200 },
-    { x: 200 - gridSize, y: 200 },
-    { x: 200 - gridSize * 2, y: 200 },
-    { x: 200 - gridSize * 3, y: 200 },
-    { x: 200 - gridSize * 4, y: 200 }
-]
+let snake = 0;
 
 let dx = 20;
 let dy = 0;
+
+function init() {
+    snake = [
+        { x: 200, y: 400 },
+        { x: 200 - gridSize, y: 400 },
+        { x: 200 - gridSize * 2, y: 400 },
+        { x: 200 - gridSize * 3, y: 400 },
+        { x: 200 - gridSize * 4, y: 400 }
+    ]
+    
+    dx = 20;
+    dy = 0;
+}
+var gridSize = 20;
+var updateTime = 100;
+
+
+init();
 main();
 
 // main game loop that gets called with interval 'updateTime'
 function main() {
     setTimeout(function onTick() {
-        clearCanvas();
-        move();
-        draw();
-        main();
+        if (gameOver()) {
+            waitForReset();
+        }
+        else {
+            clearCanvas();
+            move();
+            draw();
+            main();
+        }
     }, updateTime)
+
     updateTime = snake.length * 5 + 50;
 }
 function move() {
@@ -48,20 +68,20 @@ function move() {
 //keypresses
 function change_direction(event) {
     const keyPressed = event.keyCode;
-    
-    if (keyPressed == KEY_LEFT && dx != 20) {
+
+    if (keyPressed == KEY_LEFT || keyPressed == KEY_A && dx != 20) {
         dx = -20;
         dy = 0;
     }
-    if (keyPressed == KEY_RIGHT && dx != -20) {
+    if (keyPressed == KEY_RIGHT || keyPressed == KEY_D && dx != -20) {
         dx = 20;
         dy = 0;
     }
-    if (keyPressed == KEY_UP && dy != 20) {
+    if (keyPressed == KEY_UP || keyPressed == KEY_W && dy != 20) {
         dx = 0;
         dy = -20;
     }
-    if (keyPressed == KEY_DOWN && dy != -20) {
+    if (keyPressed == KEY_DOWN || keyPressed == KEY_S && dy != -20) {
         dx = 0;
         dy = 20;
     }
@@ -71,16 +91,29 @@ function change_direction(event) {
 
 };
 
+function gameOver() {
+
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+            console.log("AVSLUTA1");
+            return true;
+        }
+
+    }
+    if (snake[0].x < 0 || snake[0].x > board.width - gridSize || snake[0].y < 0 || snake[0].y > board.height - gridSize) {
+        console.log("AVSLUTA2");
+        return true;
+    }
+
+    return false;
+}
+
 
 // draw a border around the canvas
 function clearCanvas() {
-    //  Select the colour to fill the drawing
     board_ctx.fillStyle = board_background;
-    //  Select the colour for the border of the canvas
     board_ctx.strokestyle = board_border;
-    // Draw a "filled" rectangle to cover the entire canvas
     board_ctx.fillRect(0, 0, board.width, board.height);
-    // Draw a "border" around the entire canvas
     board_ctx.strokeRect(0, 0, board.width, board.height);
 }
 
@@ -91,7 +124,18 @@ function drawSnakePart(snakePart) {
     board_ctx.strokeRect(snakePart.x, snakePart.y, gridSize, gridSize);
 }
 
-/*Function that prints the parts*/
+
 function draw() {
     snake.forEach(drawSnakePart);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+async function waitForReset() {
+    await sleep(2000);
+    init();
+    main();
 }
