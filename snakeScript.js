6444cc89064d328209dbf8d17f62a26c94ca53f3
Emil Jönsson <ungothar97@gmail.com>
2020-11-28 15:42:21 +0100

    Apped apples

--- snakeScript.js
+++ snakeScript.js
@@ -8,6 +8,7 @@ const board = document.getElementById("gameCanvas");
 const board_ctx = gameCanvas.getContext("2d");
 
 
+
 const KEY_LEFT = 37;
 const KEY_UP = 38;
 const KEY_RIGHT = 39;
@@ -20,8 +21,13 @@ const KEY_D = 68;
 
 let snake = 0;
 
-let dx = 20;
+let dx = gridSize;
 let dy = 0;
+
+let appleX = 100;
+let appleY = 200;
+var gridSize = 40;
+var updateTime = 100;
 
 function init() {
     snake = [
@@ -31,12 +37,13 @@ function init() {
         { x: 200 - gridSize * 3, y: 400 },
         { x: 200 - gridSize * 4, y: 400 }
     ]
-    
-    dx = 20;
+
+
+    spawnApple();
+    dx = gridSize;
     dy = 0;
 }
-var gridSize = 20;
-var updateTime = 100;
+
 
 
 init();
@@ -56,12 +63,18 @@ function main() {
         }
     }, updateTime)
 
-    updateTime = snake.length * 5 + 50;
+    updateTime = 150 - snake.length*2;
 }
 function move() {
     var head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    console.log(head.x + "   " + head.y);
     snake.unshift(head);
-    snake.pop();
+    if (head.x == appleX && head.y == appleY) {
+        spawnApple();
+    }
+    else {
+        snake.pop();
+    }
 }
 
 
@@ -69,21 +82,21 @@ function move() {
 function change_direction(event) {
     const keyPressed = event.keyCode;
 
-    if (keyPressed == KEY_LEFT || keyPressed == KEY_A && dx != 20) {
-        dx = -20;
+    if (keyPressed == KEY_LEFT || keyPressed == KEY_A && dx != gridSize) {
+        dx = -gridSize;
         dy = 0;
     }
-    if (keyPressed == KEY_RIGHT || keyPressed == KEY_D && dx != -20) {
-        dx = 20;
+    if (keyPressed == KEY_RIGHT || keyPressed == KEY_D && dx != -gridSize) {
+        dx = gridSize;
         dy = 0;
     }
-    if (keyPressed == KEY_UP || keyPressed == KEY_W && dy != 20) {
+    if (keyPressed == KEY_UP || keyPressed == KEY_W && dy != gridSize) {
         dx = 0;
-        dy = -20;
+        dy = -gridSize;
     }
-    if (keyPressed == KEY_DOWN || keyPressed == KEY_S && dy != -20) {
+    if (keyPressed == KEY_DOWN || keyPressed == KEY_S && dy != -gridSize) {
         dx = 0;
-        dy = 20;
+        dy = gridSize;
     }
 
 
@@ -95,13 +108,11 @@ function gameOver() {
 
     for (let i = 1; i < snake.length; i++) {
         if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
-            console.log("AVSLUTA1");
             return true;
         }
 
     }
     if (snake[0].x < 0 || snake[0].x > board.width - gridSize || snake[0].y < 0 || snake[0].y > board.height - gridSize) {
-        console.log("AVSLUTA2");
         return true;
     }
 
@@ -123,15 +134,31 @@ function drawSnakePart(snakePart) {
     board_ctx.fillRect(snakePart.x, snakePart.y, gridSize, gridSize);
     board_ctx.strokeRect(snakePart.x, snakePart.y, gridSize, gridSize);
 }
+function drawApple() {
+    board_ctx.fillStyle = 'red';
+    board_ctx.strokestyle = 'red';
+    board_ctx.fillRect(appleX, appleY, gridSize, gridSize);
+    board_ctx.strokeRect(appleX, appleY, gridSize, gridSize);
+}
 
 
 function draw() {
+
     snake.forEach(drawSnakePart);
+    drawApple();
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
+
+function spawnApple() {
+    appleX = getRandomPos(0, board.width - gridSize);
+    appleY = getRandomPos(0, board.height - gridSize);
+}
+function getRandomPos(min, max) {
+    return Math.round((Math.random() * (max - min) + min) / gridSize) * gridSize;
+}
 
 
 async function waitForReset() {
